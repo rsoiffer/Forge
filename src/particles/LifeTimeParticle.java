046834314fadb87a6b2620e11b4f0d0f4d1bb9fa
Ian Sedgewick <Ian Sedgewick@DESKTOP-CRTF5JU>
2016-06-13 14:54:27 -0700

    Documented Particle and LifeTimeParticle

--- src/particles/LifeTimeParticle.java
+++ src/particles/LifeTimeParticle.java
@@ -4,21 +4,54 @@ import util.Vec2;
 
 public class LifeTimeParticle extends Particle {
 
-    public double lifeTime, maxLifeTime;
+    /**
+     * How long the particle has been alive
+     */
+    public double lifeTime;
+    /**
+     * Warrants how long the particle is allowed to exist in the game
+     */
+    public double maxLifeTime;
 
+    /**
+     * Constructor for LifeTimeParticle. Sets the position, velocity and the
+     * maximum time it can be alive
+     *
+     * @param pos
+     * @param vel
+     * @param maxLifeTime
+     */
     public LifeTimeParticle(Vec2 pos, Vec2 vel, double maxLifeTime) {
         super(pos, vel);
         this.maxLifeTime = maxLifeTime;
     }
 
+    /**
+     * Returns the the percent of time the particle will be alive
+     *
+     * @return
+     */
     public double percTimeRemaining() {
         return 1 - lifeTime / maxLifeTime;
     }
 
+    /**
+     * Returns the time the particle has remaining
+     *
+     * @return
+     */
     public double timeRemaining() {
         return maxLifeTime - lifeTime;
     }
 
+    /**
+     * Updates the position of the particle with delta time, dt. The delta time
+     * is updated to be equal to the sum of delta time and lifeTime. The
+     * particle's shouldRemove boolean is set to true if lifeTime is greater
+     * than maxLifeTime
+     *
+     * @param dt
+     */
     @Override
     public void update(double dt) {
         super.update(dt);
--- src/particles/Particle.java
+++ src/particles/Particle.java
@@ -4,18 +4,40 @@ import util.Vec2;
 
 public class Particle {
 
-    public Vec2 pos;
+    /**
+     * The position of the particle 
+     */
+    public Vec2 pos; 
+    /**
+     * The velocity of the particle
+     */
     public Vec2 vel;
+    /**
+     * A boolean that determines if this particles should be removed from the game world
+     */
     public boolean shouldRemove;
 
+    /**
+     * The constructor for the Particle class.
+     * Sets the position and velocity for new particle
+     * @param pos
+     * @param vel 
+     */
     public Particle(Vec2 pos, Vec2 vel) {
         this.pos = pos;
         this.vel = vel;
     }
 
+    /**
+     * At first the method is blank and void. Will be overwritten in what ever extends Particle, so render will very on the class that extends it
+     */
     public void render() {
     }
 
+    /**
+     * Changes the position of the particle by adding to the current position the current velocity and delta time
+     * @param dt 
+     */
     public void update(double dt) {
         pos = pos.add(vel.multiply(dt));
     }
